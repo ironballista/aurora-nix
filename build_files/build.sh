ba79c00399ba92a1eb245dc58b5e098fea7b38a0
ironballista <87903825+ironballista@users.noreply.github.com>
2025-10-24 14:34:12 +0200

    Added niri package and repo.

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,6 +19,9 @@ dnf install -y kitty
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
  
+sudo dnf -ycopr enable yalter/niri
+sudo dnf -y install niri
+
 sudo dnf -y copr enable alternateved/keyd
 sudo dnf -y install keyd
 
